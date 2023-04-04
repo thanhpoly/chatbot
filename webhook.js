492a91e2e7a41bddc762f7080aeee1d3ecc18c47
Thang Le <thang.le@datx.com.vn>
2023-04-04 15:11:20 +0700

    1

--- webhook.js
+++ webhook.js
@@ -144,28 +144,31 @@ const profile = (req, res) => {
 
 const getUserProfile = async (sender_psid) => {
   let username = "";
-  await request(
-    {
-      uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
-      qs: { access_token: PAGE_ACCESS_TOKEN },
-      method: "GET",
-    },
-    (err, res, body) => {
-      if (!err) {
-        response = JSON.parse(res);
-        console.log(response);
+  const callAPi = () => {
+    return new Promise((resolve, reject) => {
+      request(
+        {
+          uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+          qs: { access_token: PAGE_ACCESS_TOKEN },
+          method: "GET",
+        },
+        (err, res, body) => {
+          if (!err) {
+            response = JSON.parse(res);
+            console.log(response);
 
-        username = `${response.first_name} ${response.last_name}`;
-        console.log("username111111111", username);
-      } else {
-        console.error("Unable to send message:" + err);
-      }
-    }
-  );
-
-  const aaa = username;
-  console.log("aaaaaa", aaaa);
-  return aaa;
+            username = `${response.first_name} ${response.last_name}`;
+            console.log("username111111111", username);
+            resolve();
+          } else {
+            console.error("Unable to send message:" + err);
+          }
+        }
+      );
+    });
+  };
+  await callAPi();
+  return username;
 };
 
 const handleGetStarted = (sender_psid) => {
